afe74a31d76b1a02a4abb76458cb81c09e1b5b77
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-06-26 15:19:27 +0200

    stats are now shown on the pokemon card

--- src/pages/pokemon/Pokemon.jsx
+++ src/pages/pokemon/Pokemon.jsx
@@ -1,5 +1,26 @@
 import { useLoaderData, useParams, Link } from "react-router-dom";
 
+const typeColors = {
+    normal: "bg-gray-300",
+    fire: "bg-red-500",
+    water: "bg-blue-500",
+    grass: "bg-green-500",
+    electric: "bg-yellow-400",
+    ice: "bg-blue-200",
+    fighting: "bg-red-700",
+    poison: "bg-purple-500",
+    ground: "bg-yellow-800",
+    flying: "bg-indigo-400",
+    psychic: "bg-pink-400",
+    bug: "bg-green-600",
+    rock: "bg-gray-700",
+    ghost: "bg-purple-700",
+    dragon: "bg-indigo-700",
+    dark: "bg-gray-800",
+    steel: "bg-gray-500",
+    fairy: "bg-pink-200",
+};
+
 export default function Pokemon() {
     const { id } = useParams();
     const pokemon = useLoaderData();
@@ -11,14 +32,14 @@ export default function Pokemon() {
             <div className="flex gap-2 justify-center text-3xl mb-10 mt-3">
                 <h2>{pokemon.name}</h2> <h2 className="text-gray-500">#{id}</h2>
             </div>
-            <div className="flex-col flex items-center bg-red-500">
+            <div className="flex-col flex items-center ">
                 <div className="flex flex-col items-center">
                     <img
                         src={pokemon.sprites.front_default}
                         className="w-96"
                         alt=""
                     />
-                    <div className="bg-blue-500 rounded-lg p-4 w-80  h-48 flex flex-col justify-evenly text-white ">
+                    <div className="bg-blue-500 rounded-lg p-4 w-80 h-48 flex flex-col justify-evenly text-white ">
                         <div className="flex gap-5">
                             <div className="mb-2">
                                 <span className="font-bold">Height: </span>
@@ -39,20 +60,42 @@ export default function Pokemon() {
                         </div>
                     </div>
                 </div>
-                <div className="flex flex-col ">
-                    <p>Type</p>
-                    <p className="bg-orange-500 px-4 rounded-md">Fire</p>
+                <div className="flex flex-col items-center">
+                    <p>Type:</p>
+                    <div className="gap-2 flex">
+                        {pokemon.types.map((type) => (
+                            <p
+                                key={type.slot}
+                                className={`px-4 rounded-md ${
+                                    typeColors[type.type.name]
+                                }`}
+                            >
+                                {type.type.name}
+                            </p>
+                        ))}
+                    </div>
                 </div>
-                Weaknesses
-                <p className="bg-orange-500 px-4 rounded-md">Fire</p>
-                Stats
-                <div className="flex gap-2 ">
-                    <p>HP</p>
-                    <p>Attack</p>
-                    <p>Defense</p>
-                    <p>Special Attack</p>
-                    <p>Special Defense</p>
-                    <p>Speed</p>
+                <div className="stats-table">
+                    <table className="w-full">
+                        <thead>
+                            <tr>
+                                <th className="text-left">Stat</th>
+                                <th className="text-right">Value</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            {pokemon.stats.map((stat) => (
+                                <tr key={stat.stat.name}>
+                                    <td className="text-left">
+                                        {stat.stat.name}
+                                    </td>
+                                    <td className="text-right">
+                                        {stat.base_stat}
+                                    </td>
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
                 </div>
             </div>
             <div className="bg-green-500 flex justify-between p-4">
