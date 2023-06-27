97096baabf4cd00a4072c261131e8364cd37b8ae
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-06-27 14:22:48 +0200

    search bar functions perfectly

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,25 +1,48 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import PokemonCard from "../components/PokemonCard";
 import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
-    const allPokemons = useLoaderData();
     const [searchQuery, setSearchQuery] = useState("");
+    const [allPokemons, setAllPokemons] = useState([]);
+    const [filteredPokemons, setFilteredPokemons] = useState([]);
+
+    useEffect(() => {
+        const fetchPokemonData = async () => {
+            try {
+                const response = await fetch(
+                    "https://pokeapi.co/api/v2/pokemon?limit=1010"
+                );
+                if (!response.ok) {
+                    throw new Error("Failed to fetch Pokemon data");
+                }
+                const data = await response.json();
+                setAllPokemons(data.results);
+            } catch (error) {
+                console.error(error);
+            }
+        };
+
+        fetchPokemonData();
+    }, []);
+
+    useEffect(() => {
+        const formattedSearchQuery = searchQuery.toLowerCase();
+        const filteredResults = allPokemons.filter((pokemon) => {
+            const { name, id } = pokemon;
+            const formattedName = name.toLowerCase();
+            const formattedId = String(id).padStart(3, "0");
+            return (
+                formattedName.includes(formattedSearchQuery) ||
+                formattedId.includes(formattedSearchQuery)
+            );
+        });
+        setFilteredPokemons(filteredResults.slice(0, 70));
+    }, [searchQuery, allPokemons]);
 
     const handleSearch = (e) => {
         setSearchQuery(e.target.value);
     };
-
-    const filteredPokemons = allPokemons.results.filter((pokemon) => {
-        const { name, id } = pokemon;
-        const formattedId = String(id).toLowerCase();
-        const formattedSearchQuery = searchQuery.toLowerCase();
-
-        return (
-            name.includes(formattedSearchQuery) ||
-            formattedId.includes(formattedSearchQuery)
-        );
-    });
 
     return (
         <div className="bg-black">
@@ -38,7 +61,7 @@ export default function Home() {
                     </form>
                 </div>
                 <div className="">
-                    <div className="grid grid-cols-7 gap-5 mt-2 mx-auto justify-items-center ">
+                    <div className="grid grid-cols-7 gap-5 mt-2 mx-auto justify-items-center">
                         {filteredPokemons.map((pokemon, index) => (
                             <PokemonCard key={index} {...pokemon} />
                         ))}
@@ -50,7 +73,7 @@ export default function Home() {
 }
 
 export const getFirst20Pokemon = async () => {
-    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=70");
+    const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
     if (!res.ok) {
         throw Error("Failed to fetch Pokemon data");
     }
