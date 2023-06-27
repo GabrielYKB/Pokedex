2d44fa55c49abe4472941914a29ea5ff3c1cf6fe
Gabriel Yakoub <gabriel.yakooub@gmail.com>
2023-06-27 13:40:32 +0200

    added search bar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,13 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 import PokemonCard from "../components/PokemonCard";
 import { useLoaderData } from "react-router-dom";
 
 export default function Home() {
-    const pokemons = useLoaderData();
-    console.log(pokemons);
+    const allPokemons = useLoaderData();
+    const [searchQuery, setSearchQuery] = useState("");
+
+    const handleSearch = (e) => {
+        setSearchQuery(e.target.value);
+    };
+
+    const filteredPokemons = allPokemons.results.filter((pokemon) => {
+        const { name, id } = pokemon;
+        const formattedId = String(id).toLowerCase();
+        const formattedSearchQuery = searchQuery.toLowerCase();
+
+        return (
+            name.includes(formattedSearchQuery) ||
+            formattedId.includes(formattedSearchQuery)
+        );
+    });
+
     return (
         <div className="bg-black">
-            <div className="bg-white container mx-auto">
+            <div className="bg-white container mx-auto px-5">
                 <div className="bg-red-800">
                     <form action="">
                         <input
@@ -15,13 +31,16 @@ export default function Home() {
                             type="text"
                             name=""
                             id=""
+                            value={searchQuery}
+                            onChange={handleSearch}
+                            placeholder="Search Pokemon"
                         />
                     </form>
                 </div>
                 <div className="">
                     <div className="grid grid-cols-7 gap-5 mt-2 mx-auto justify-items-center ">
-                        {pokemons.results.map((d, i) => (
-                            <PokemonCard key={i} {...d} />
+                        {filteredPokemons.map((pokemon, index) => (
+                            <PokemonCard key={index} {...pokemon} />
                         ))}
                     </div>
                 </div>
@@ -29,10 +48,11 @@ export default function Home() {
         </div>
     );
 }
+
 export const getFirst20Pokemon = async () => {
     const res = await fetch("https://pokeapi.co/api/v2/pokemon?limit=70");
     if (!res.ok) {
-        throw Error("Det gick inte att hämta profilerna");
+        throw Error("Failed to fetch Pokemon data");
     }
 
     return res.json();
